b89fd5d78c4521d9c0697e714d236eaaed3d649c
BryanNearing <bryannearing@gmail.com>
2021-01-22 14:12:55 -0600

    add hibernate configuration

--- src/main/java/com/TaskManagerApplication.java
+++ src/main/java/com/TaskManagerApplication.java
@@ -1,5 +1,6 @@
 package com;
 
+import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TaskManagerApplication {
 
 	public static void main(String[] args) {
+		
+		Configuration cfg=new Configuration();    
+		cfg.configure("hibernate.cfg.xml"); 
+		
 		SpringApplication.run(TaskManagerApplication.class, args);
 	}
 
