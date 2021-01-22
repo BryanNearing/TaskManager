a4d38ece955a684ce64fd6dfee61f5659445856c
BryanNearing <bryannearing@gmail.com>
2021-01-22 15:58:17 -0600

    fixing hibernate

--- src/main/java/com/TaskManagerApplication.java
+++ src/main/java/com/TaskManagerApplication.java
@@ -1,5 +1,10 @@
 package com;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,8 +14,17 @@ public class TaskManagerApplication {
 
 	public static void main(String[] args) {
 		
-		Configuration cfg=new Configuration();    
-		cfg.configure("hibernate.cfg.xml"); 
+		Map<String,String> jdbcUrlSettings = new HashMap<>();
+		String jdbcDbUrl = System.getenv("JDBC_DATABASE_URL");
+		if (null != jdbcDbUrl) {
+		  jdbcUrlSettings.put("hibernate.connection.url", System.getenv("JDBC_DATABASE_URL"));
+		}
+
+		StandardServiceRegistry registry = new StandardServiceRegistryBuilder().
+		    configure("hibernate.cfg.xml").
+		    applySettings(jdbcUrlSettings).
+		    build();
+
 		
 		SpringApplication.run(TaskManagerApplication.class, args);
 	}
